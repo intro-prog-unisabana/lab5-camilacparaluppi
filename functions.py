c24b56b78b431c230097191311695fc9a1a38aba
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-10 16:45:01 -0500

    problema1.py

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 def promedio_estudiante(calificaciones):
+    if len(calificaciones) == 0:
+        return 0.0
     return sum(calificaciones) / len(calificaciones)
 calificaciones = [85, 92, 78]
 prom = promedio_estudiante(calificaciones)
