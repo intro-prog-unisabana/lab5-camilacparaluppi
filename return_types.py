718be7e2ef2e567f8f9d226b0bc76016246081c9
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-10 16:50:29 -0500

    problema2.py

---
+++ return_types.py
@@ -0,0 +1,6 @@
+entrada = input("Enter the item's price:\n")
+def obtener_precio_usuario():
+    """El usuario debe ingresar un precio"""
+    return entrada
+precio = obtener_precio_usuario()
+print(float(precio))
