46ca71118c115db6a1efa362bf144c9b4c55407b
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-13 11:17:45 -0500

    problema7.py

--- random_q.py
+++ random_q.py
@@ -1,6 +1,6 @@
 import random
 random.seed(123)
-inicio_rango = input("Enter the start value:\n")
-final_rango = input("Enter the start value:\n")
+inicio_rango = int(input("Enter the start value:\n"))
+final_rango = int(input("Enter the end value:\n"))
 numero_aleatorio = random.randint(inicio_rango, final_rango)
 print(f"Generated random number: {numero_aleatorio}")
