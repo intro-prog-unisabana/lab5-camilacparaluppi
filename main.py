ad6dd903d2ffa661c07ac3acc9f0c60263558dcb
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-14 12:04:56 -0500

    problema10.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from utils import add, sub, multiply, divide, exponent, modulo, floor_divide, absolute
 def main():
     while True:
-        operacion = input("Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit): ").lower()
+        operacion = input(
+            "Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit): ").lower()
         if operacion == "exit":
             break
-        if operacion not in ["add","subtract","multiply","divide","exponent","modulo","floor_divide","absolute"]:
+        if operacion not in ["add", "subtract", "multiply", "divide", "exponent", "modulo", "floor_divide", "absolute"]:
             print("Invalid option!")
             continue
         if operacion == "absolute":
@@ -28,8 +29,11 @@ def main():
                 result = modulo(num1, num2)
             elif operacion == "floor_divide":
                 result = floor_divide(num1, num2)
-        
-        print(f"The result is: {result}")
+
+        if isinstance(result, str):
+            print(result)
+        else:
+            print(f"The result is: {result}")
 
 if __name__ == "__main__":
     main()
