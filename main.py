72642b24493c1bf35716a0a85ad7c802920d81a9
Camila C <camilacparaluppiiiii@gmail.com>
2026-03-14 11:55:16 -0500

    problema10.py

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 from utils import add, sub, multiply, divide, exponent, modulo, floor_divide, absolute
-while True:
-    operacion = input("Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit): ").lower()
-    if operacion == "exit":
-        break
-    if operacion not in ["add", "subtract", "multiply", "divide", "exponent", "modulo", "floor_divide", "absolute"]:
-        print("Invalid option!")
-        continue
-    if operacion == "absolute":
-        num = float(input("Enter the number: "))
-        result = absolute(num)
-    else:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
+def main():
+    while True:
+        operacion = input("Which calculation would you like to perform? (add, subtract, multiply, divide, exponent, modulo, floor_divide, absolute, exit): ").lower()
+        if operacion == "exit":
+            break
+        if operacion not in ["add","subtract","multiply","divide","exponent","modulo","floor_divide","absolute"]:
+            print("Invalid option!")
+            continue
+        if operacion == "absolute":
+            num = float(input("Enter the number: "))
+            result = absolute(num)
+        else:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
 
-        if operacion == "add":
-            result = add(num1, num2)
-        elif operacion == "subtract":
-            result = sub(num1, num2)
-        elif operacion == "multiply":
-            result = multiply(num1, num2)
-        elif operacion == "divide":
-            result = divide(num1, num2)
-        elif operacion == "exponent":
-            result = exponent(num1, num2)
-        elif operacion == "modulo":
-            result = modulo(num1, num2)
-        elif operacion == "floor_divide":
-            result = floor_divide(num1, num2)
-            
-    print(f"The result is: {result}")
+            if operacion == "add":
+                result = add(num1, num2)
+            elif operacion == "subtract":
+                result = sub(num1, num2)
+            elif operacion == "multiply":
+                result = multiply(num1, num2)
+            elif operacion == "divide":
+                result = divide(num1, num2)
+            elif operacion == "exponent":
+                result = exponent(num1, num2)
+            elif operacion == "modulo":
+                result = modulo(num1, num2)
+            elif operacion == "floor_divide":
+                result = floor_divide(num1, num2)
+        
+        print(f"The result is: {result}")
+
+if __name__ == "__main__":
+    main()
